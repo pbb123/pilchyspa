7217126a782d901897092569b3419afad3bfeb37
pbb <robot.pikacz3@gmail.com>
2017-10-08 18:13:03 +0200

    Zmiany w ustawieniach(static)

--- pilchyspasite/settings.py
+++ pilchyspasite/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/rezerwacje/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
