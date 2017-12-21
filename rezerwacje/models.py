4757cdf5061dc6a113a6b34c9697429ecc58f187
pbb <robot.pikacz3@gmail.com>
2017-12-21 19:17:46 +0100

    Django 2.0. Poprawki w rooms.html

--- rezerwacje/models.py
+++ rezerwacje/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Rodzina(models.Model):
-    user=models.ForeignKey(User)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     liczebność=models.IntegerField()
     def __str__(self):
         return str(self.user)
@@ -17,15 +17,15 @@ class Pokoj(models.Model):
 class Rezerwacja(models.Model):
      od=models.IntegerField()
      do=models.IntegerField()
-     pokoj=models.ForeignKey(Pokoj)
-     rodzina=models.ForeignKey(Rodzina)
+     pokoj=models.ForeignKey(Pokoj,on_delete=models.CASCADE)
+     rodzina=models.ForeignKey(Rodzina,on_delete=models.CASCADE)
      rozmiar=models.IntegerField(null=True)
      limit=models.NullBooleanField()
      def __str__(self):
          return "Od: "+str(self.od)+" Do: "+str(self.do)+" Przez: "+str(self.rodzina)+" W pokoju: "+str(self.pokoj)
     
 class Error(models.Model):
-    rodzina=models.ForeignKey(Rodzina)
+    rodzina=models.ForeignKey(Rodzina,on_delete=models.CASCADE)
     opis=models.TextField()
     
 class Day(models.Model):
